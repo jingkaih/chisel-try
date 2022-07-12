43c4a0023dcd28ecee6a457d8ec464d8b5ee6ee5
jingkaih <jingkaih@usc.edu>
2022-07-11 21:16:27 -0700

    add 2 pointers for outBuf; add rollback; add datapath; remove deprecated code

--- src/main/scala/BooleanNew/BP.scala
+++ src/main/scala/BooleanNew/BP.scala
@@ -37,9 +37,9 @@ class BP(PEcolCnt: Int = 21, dataWidth: Int = 64, dataRAMaddrWidth: Int = 8, Tag
     val wr_Tag_inBuf = Input(new MEMTagDataBundle(TagWidth, CounterWidth))// attached to wr_D_inBuf
     val wr_Addr_inBuf_en = Input(Bool())// write pointer increment enable
     //data memory interface (output)
-    val rd_Addr_outBuf = Input(UInt(8.W))
-    val rd_D_outBuf = Vec(64, Output(new MEMDataBundle(dataWidth)))
-    val PC_out = Output(UInt(8.W))
+//    val rd_Addr_outBuf = Input(UInt(8.W))
+//    val rd_D_outBuf = Vec(64, Output(new MEMDataBundle(dataWidth)))
+//    val PC_out = Output(UInt(8.W))
   })
 
   val banks = Vec(64, new MEMDataBundle(dataWidth))
@@ -62,22 +62,6 @@ class BP(PEcolCnt: Int = 21, dataWidth: Int = 64, dataRAMaddrWidth: Int = 8, Tag
     wr_Addr_inBuf := wr_Addr_inBuf + 1.U
     wr_Addr_inBuf_1 := wr_Addr_inBuf_1 + 1.U
   }
-
-
-  //output buffer read
-//  val rd_D_outBuf = Vec(64, Reg(new MEMDataBundle()))
-//  rd_D_outBuf := outputDataBuffer(io.rd_Addr_outBuf)
-//  io.rd_D_outBuf := rd_D_outBuf
-  io.rd_D_outBuf := outputDataBuffer.read(io.rd_Addr_outBuf)
-
-
-
-
-
-
-
-
-
 
 
 
@@ -103,9 +87,18 @@ class BP(PEcolCnt: Int = 21, dataWidth: Int = 64, dataRAMaddrWidth: Int = 8, Tag
   val rd_D_inBuf_RndCnt_decre = Reg(Vec(64, new MEMDataBundle(dataWidth)))
   val rd_Tag_inBuf_RndCnt_decre = Reg(new MEMTagDataBundle(TagWidth, CounterWidth))
 
-  rd_D_inBuf_RndCnt_decre := rd_D_inBuf
-  rd_Tag_inBuf_RndCnt_decre.Tag := rd_Tag_inBuf.Tag
-  rd_Tag_inBuf_RndCnt_decre.RoundCnt := rd_Tag_inBuf.RoundCnt - 1.U
+  // 1.1 bring this data to the front
+  when(!roll_back){
+    rd_D_inBuf_RndCnt_decre := rd_D_inBuf
+    rd_Tag_inBuf_RndCnt_decre.Tag := rd_Tag_inBuf.Tag
+    rd_Tag_inBuf_RndCnt_decre.RoundCnt := rd_Tag_inBuf.RoundCnt - 1.U
+  }.otherwise{
+    rd_D_inBuf_RndCnt_decre := rd_D_outBuf
+    rd_Tag_inBuf_RndCnt_decre.Tag := rd_Tag_outBuf.Tag
+    rd_Tag_inBuf_RndCnt_decre.RoundCnt := rd_Tag_outBuf.RoundCnt - 1.U
+  }
+
+
 
 
   rd_D_inBuf := inputDataBuffer(rd_Addr_inBuf)
@@ -142,19 +135,20 @@ class BP(PEcolCnt: Int = 21, dataWidth: Int = 64, dataRAMaddrWidth: Int = 8, Tag
   }. otherwise{
     context_switch := 0.B
   }
-  when(context_switch){
+
+  //feed back data path part 1
+  when(rd_Addr_outBuf_pointer === rd_Addr_outBuf) {
+    roll_back := 0.B
+  } .elsewhen((context_switch && allValidBitsPopCnt =/= 0.U && wr_Tag_outBuf_reg.RoundCnt =/= 0.U) || roll_back === 1.B){
+    //notice if there's only context_switch && wr_Tag_outBuf_reg.RoundCnt =/= 0.U, then when we context switch from tag00 graph (trash PIs) to valid graph, it will also trigger
     roll_back := 1.B
   }
 
 
 
-
-//  val wr_TagOld_outBuf = RegInit(0.U(2.W))
-
-
   val Addr_out = Wire(UInt(8.W))// the actual addr stream out from the last building block, not always useful
   val Tag_out = Wire(new MEMTagDataBundle(TagWidth, CounterWidth))
-
+  val PC_out = Wire(UInt(8.W))
 //  wr_Addr_outBuf := Addr_out// self-increment
   wr_Tag_outBuf := Tag_out
 
@@ -229,46 +223,70 @@ class BP(PEcolCnt: Int = 21, dataWidth: Int = 64, dataRAMaddrWidth: Int = 8, Tag
   )
   val allValidBitsPopCnt = RegInit(0.U(6.W))
   allValidBitsPopCnt := PopCount(allValidBits)
-  //output buffer starts writing data
+
+  val rd_D_outBuf = Reg(Vec(64, new MEMDataBundle(dataWidth)))
+  val rd_Tag_outBuf = Reg(new MEMTagDataBundle(TagWidth, CounterWidth))
+  val rd_Addr_outBuf := RegInit(0.U(8.W))
+  val rd_Addr_outBuf_1 := RegInit(0.U(8.W))
+  val rd_Addr_outBuf_pointer := RegInit(0.U(8.W))
+  val rd_Addr_outBuf_pointer_1 := RegInit(0.U(8.W))
+
   val wr_Addr_outBuf = RegInit(0.U(8.W)) // the 1st incrementor
   val wr_Addr_outBuf_1 = RegInit(0.U(8.W)) // the 2nd incrementor, at same rate
-//  val wr_AddrReg_outBuf = RegNext(wr_Addr_outBuf)
-//  val wr_AddrFlwUp_outBuf = RegInit(0.U(8.W))// follow-up write pointer; update not in every cycle
+  val wr_Addr_outBuf_pointer = RegInit(0.U(8.W)) //points to the next location at which THE ultimate output will appear; Only if allValidBitsPopCnt =/=0.U and RoundCnt === 0, we update this because the current wr_D_outBuf is the final result
+  val wr_Addr_outBuf_pointer_1 = RegInit(0.U(8.W))
+  //This wr_Addr_outBuf_pointer is also used as part of the roll back mechanism
+  //The intermediate results are stored beyond this location but temporarily
+  //once roll back
+  // 1. start fetching data (intermediate result) from outBuf between location wr_Addr_outBuf_pointer (smaller) and wr_Addr_outBuf (greater)
+  // 1.1 bring this data to the front
+  // 2. and roll wr_Addr_outBuf back to wr_Addr_outBuf_pointer; in roll_back duration, set wr_Addr_outBuf in every clk (i.e., freeze it at wr_Addr_outBuf_pointer)
+  //when can we start incrementing it again? the time allValidBitsPopCnt =/= 0.U again
+  // 3. roll back PC as well. To PCout??
+  // 4. rollback rd_Addr_inBuf and rd_Addr_inBuf_1 to Addrout??
 
-  when(allValidBitsPopCnt =/= 0.U){
-    wr_Addr_outBuf := wr_Addr_outBuf + 1.U
+  when(roll_back){
+    //2.
+    wr_Addr_outBuf := wr_Addr_outBuf_pointer
+    wr_Addr_outBuf_1 := wr_Addr_outBuf_pointer_1
+  }.elsewhen(allValidBitsPopCnt =/= 0.U){
+    wr_Addr_outBuf := wr_Addr_outBuf + 1.U // increment to the next empty location
     wr_Addr_outBuf_1 := wr_Addr_outBuf_1 + 1.U
     outputDataBuffer.write(wr_Addr_outBuf, wr_D_outBuf_reg)
     outputTagBuffer.write(wr_Addr_outBuf_1, wr_Tag_outBuf_reg)
+  }.otherwise{
+    wr_Addr_outBuf := wr_Addr_outBuf
+    wr_Addr_outBuf_1 := wr_Addr_outBuf_1
+  }
+
+  when(allValidBitsPopCnt =/= 0.U && wr_Tag_outBuf_reg.RoundCnt === 0.U){//notice roundCnt of trash PIs is 0 but the allValidBitsPopCnt will also be 0, No harm
+    //to understand it, pls compare its condition with wr_Addr_outBuf's incrementing condition
+    wr_Addr_outBuf_pointer := wr_Addr_outBuf_pointer + 1.U// always point to the next location that is ready to store a final result
+    wr_Addr_outBuf_pointer_1 := wr_Addr_outBuf_pointer_1 + 1.U
+  }
+
+
+  //1.
+  // read Addr and read Addr pointer both take the snapshot of the current write Addr
+  when(context_switch && allValidBitsPopCnt =/= 0.U && wr_Tag_outBuf_reg.RoundCnt =/= 0.U) { // this is same as the rollback enable condition, but it will initate operation in the bracket for only 1 clk
+    //the below statements happen 1 clk before the statements in when(roll_back){}
+    rd_Addr_outBuf := wr_Addr_outBuf// the end point
+    rd_Addr_outBuf_1 := wr_Addr_outBuf_1
+    rd_Addr_outBuf_pointer := wr_Addr_outBuf_pointer// the start point; when roll back, rd_Addr_outBuf-pointer get self incremented and when it reaches rd_Addr_outBuf_pointer, roll_back becomes 0
+    rd_Addr_outBuf_pointer_1 := wr_Addr_outBuf_pointer_1
+  }
+
+  when(roll_back){
+    //feed back data path part 2
+    rd_D_outBuf := outputDataBuffer.read(rd_Addr_outBuf_pointer)
+    rd_Tag_outBuf := outputTagBuffer.read(rd_Addr_outBuf_pointer_1)
+    rd_Addr_outBuf_pointer := rd_Addr_outBuf_pointer + 1.U
+    rd_Addr_outBuf_pointer_1 := rd_Addr_outBuf_pointer_1 + 1.U
   }
 
 
 
 
-//  val DSTWB = RegInit(false.B)//Flag of destined to be written back. Asserted when any popcount result of wr_D_outBuf who are from the same tag family is not equal to 1
-//
-//  wr_TagOld_outBuf := wr_Tag_outBuf
-//
-//  val context_switch = RegInit(true.B)
-//  when(wr_TagOld_outBuf =/= wr_Tag_outBuf){
-//    context_switch := true.B
-//    wr_AddrFlwUp_outBuf := wr_AddrReg_outBuf
-//  } .otherwise{
-//    context_switch := false.B
-//    wr_AddrFlwUp_outBuf := wr_AddrFlwUp_outBuf
-//  }
-//
-//  when(context_switch){
-//    when(allValidBitsPopCnt > 1.U){
-//      DSTWB := true.B
-//    } .otherwise{
-//      DSTWB := false.B
-//    }
-//  } .otherwise{
-//    when(allValidBitsPopCnt > 1.U){
-//      DSTWB := true.B
-//    }
-//  }
 
 
 
@@ -292,27 +310,21 @@ class BP(PEcolCnt: Int = 21, dataWidth: Int = 64, dataRAMaddrWidth: Int = 8, Tag
   }
 
 
-
-
-
-
   val PCBegin = RegInit(0.U(8.W))
   val AddrBegin = RegInit(0.U(8.W))
-  array(0).io.d_in := d_in
-  array(0).io.Tag_in := rd_Tag_inBuf_RndCnt_decre
-  array(0).io.PC1_in := PCBegin
-  array(0).io.Addr_in := AddrBegin
-
   when(beginRun_reg) { // last for only 1 cycle
     PCBegin := PCBegin + 1.U
   }
-
   when(beginRun_reg) { // last for only 1 cycle
     AddrBegin := AddrBegin + 1.U
   }
 
+  array(0).io.d_in := d_in
+  array(0).io.Tag_in := rd_Tag_inBuf_RndCnt_decre
 
-
+  //below needs to be modified
+  array(0).io.PC1_in := PCBegin
+  array(0).io.Addr_in := AddrBegin
 
 
 
@@ -355,264 +367,12 @@ class BP(PEcolCnt: Int = 21, dataWidth: Int = 64, dataRAMaddrWidth: Int = 8, Tag
   d_out := array(PEcolCnt-1).io.d_out
   Tag_out := array(PEcolCnt-1).io.Tag_out
   Addr_out := array(PEcolCnt-1).io.Addr_out
-//  PC_out := array(PEcolCnt-1).io.PC6_out
-  io.PC_out := array(PEcolCnt-1).io.PC6_out
+  PC_out := array(PEcolCnt-1).io.PC6_out
+//  io.PC_out := array(PEcolCnt-1).io.PC6_out
 
 
 }
 
-
-
-//class BP(PEcolCnt: Int = 16) extends Module{
-//  val io = IO(new Bundle{
-////    val d_in = Vec(32, Input(new PEDataBundle()))
-////    val d_out = Vec(32, Output(new PEDataBundle()))
-//
-//    //instruction memory
-//    val wr_en_mem1 = Vec(PEcolCnt, Input(Bool()))
-//    val wr_en_mem2 = Vec(PEcolCnt, Input(Bool()))
-//    val wr_en_mem3 = Vec(PEcolCnt, Input(Bool()))
-//    val wr_en_mem4 = Vec(PEcolCnt, Input(Bool()))
-//    val wr_en_mem5 = Vec(PEcolCnt, Input(Bool()))
-//    val wr_en_mem6 = Vec(PEcolCnt, Input(Bool()))
-//    val wr_instr_mem1 = Vec(PEcolCnt, Input(UInt(224.W)))//change to 192!!
-//    val wr_instr_mem2 = Vec(PEcolCnt, Input(UInt(128.W)))
-//    val wr_instr_mem3 = Vec(PEcolCnt, Input(UInt(128.W)))
-//    val wr_instr_mem4 = Vec(PEcolCnt, Input(UInt(128.W)))
-//    val wr_instr_mem5 = Vec(PEcolCnt, Input(UInt(128.W)))
-//    val wr_instr_mem6 = Vec(PEcolCnt, Input(UInt(128.W)))
-//    val beginRun = Input(Bool())// later use this to replace io.run
-//    //data memory interface (input)
-//    val wr_D_inBuf = Vec(64, Input(new MEMDataBundle()))//64*65 wide
-//    val wr_Addr_inBuf_en = Input(Bool())
-//    //data memory interface (output)
-////    val rd_Addr_outBuf = Input(UInt(8.W))
-////    val rd_D_outBuf = Vec(64, Output(new MEMDataBundle()))
-//
-//  })
-//  // input data memory declaration
-//  val banks = Vec(64, new MEMDataBundle())
-//  val inputBuffer = Mem(256, banks)
-//  // process unit declaration
-//  val array_0 = Module(new BuildingBlockNewFirst)
-//  val array = for(i <- 1 until PEcolCnt) yield Module(new BuildingBlockNew)
-//  // output data memory declaration
-//  val outputBuffer = Mem(256, banks)// each 65-wide, the MSB is valid bit
-//
-//  //input buffer initialization (write to it)
-//  val wr_Addr_inBuf = RegInit(0.U(8.W))
-//  inputBuffer(wr_Addr_inBuf) := DontCare
-//  when(io.wr_Addr_inBuf_en){
-//    inputBuffer(wr_Addr_inBuf) := io.wr_D_inBuf// continuously write data into data MEM every cycle when wr_Addr_en is 1
-//    wr_Addr_inBuf := wr_Addr_inBuf + 1.U
-//  }. otherwise{
-//    wr_Addr_inBuf := wr_Addr_inBuf
-//  }
-//
-////  //output buffer start reading
-////  val rd_D_outBuf = Vec(64, Reg(new MEMDataBundle()))
-////  rd_D_outBuf := outputBuffer(io.rd_Addr_outBuf)
-////  io.rd_D_outBuf := rd_D_outBuf
-//
-//  // depth issue handler
-//  val rollBack_en = RegInit(false.B)// last for 1 cycle
-//  val rollBack_PC = RegInit(0.U(8.W))//the last PC6 -> the first PC1
-////  val rollBack_Addr = RegInit(0.U(8.W)) // equivalent to wr_Addr_outBuf, will replace current rd_Addr_inBuf
-//  val lock = RegInit(false.B)
-//  val wr_Addr_outBuf_hold = RegInit(0.U(8.W))
-//  //input buffer starts reading data
-//  val rd_Addr_inBuf = RegInit(0.U(8.W))
-//  val rd_D_inBuf = Vec(64, Reg(new MEMDataBundle()))
-//
-//  //output buffer starts receiving data
-//  val wr_Addr_outBuf = RegInit(0.U(8.W))
-//  val wr_D_outBuf = Vec(64, Reg(new MEMDataBundle()))
-//
-//  //MEM data bundle <-> PE data bundle
-//  val d_in = Vec(32, Input(new PEDataBundle()))
-//  val d_out = Vec(32, Input(new PEDataBundle()))
-//  for(i <- 0 until 32){
-//    d_in(i).a := rd_D_inBuf(i*2).data
-//    d_in(i).valid_a := rd_D_inBuf(i*2).validBit
-//    d_in(i).b := rd_D_inBuf(i*2+1).data
-//    d_in(i).valid_b := rd_D_inBuf(i*2+1).validBit
-//  }
-//  for(i <- 0 until 32){
-//    wr_D_outBuf(i*2).data := d_out(i).a
-//    wr_D_outBuf(i*2).validBit := d_out(i).valid_a
-//    wr_D_outBuf(i*2+1).data := d_out(i).b
-//    wr_D_outBuf(i*2+1).validBit := d_out(i).valid_b
-//  }
-//
-//
-//
-//  when(io.beginRun){
-//    when(!rollBack_en){
-//      rd_D_inBuf := inputBuffer(rd_Addr_inBuf)
-//      rd_Addr_inBuf := rd_Addr_inBuf + 1.U
-//    } .otherwise{
-//      rd_Addr_inBuf := wr_Addr_outBuf// I roll back my addr to the Addr_out
-//    }
-//  }
-//
-//  array_0.io.d_in := Mux(rollBack_en ,d_out, d_in)
-//  array_0.io.wr_en_mem1 := io.wr_en_mem1(0)
-//  array_0.io.wr_en_mem2 := io.wr_en_mem2(0)
-//  array_0.io.wr_en_mem3 := io.wr_en_mem3(0)
-//  array_0.io.wr_en_mem4 := io.wr_en_mem4(0)
-//  array_0.io.wr_en_mem5 := io.wr_en_mem5(0)
-//  array_0.io.wr_en_mem6 := io.wr_en_mem6(0)
-//  array_0.io.wr_instr_mem1 := io.wr_instr_mem1(0)
-//  array_0.io.wr_instr_mem2 := io.wr_instr_mem2(0)
-//  array_0.io.wr_instr_mem3 := io.wr_instr_mem3(0)
-//  array_0.io.wr_instr_mem4 := io.wr_instr_mem4(0)
-//  array_0.io.wr_instr_mem5 := io.wr_instr_mem5(0)
-//  array_0.io.wr_instr_mem6 := io.wr_instr_mem6(0)
-//  array_0.io.run_in := io.beginRun
-//
-//  array_0.io.PC1_in_en := rollBack_en
-//  array_0.io.PC1_in := Mux(rollBack_en, rollBack_PC, 0.U)
-//  array_0.io.Addr_in := Mux(rollBack_en, wr_Addr_outBuf, rd_Addr_inBuf)
-//
-//  array(1).io.d_in := array_0.io.d_out
-//  array(1).io.wr_en_mem1 := io.wr_en_mem1(1)
-//  array(1).io.wr_en_mem2 := io.wr_en_mem2(1)
-//  array(1).io.wr_en_mem3 := io.wr_en_mem3(1)
-//  array(1).io.wr_en_mem4 := io.wr_en_mem4(1)
-//  array(1).io.wr_en_mem5 := io.wr_en_mem5(1)
-//  array(1).io.wr_en_mem6 := io.wr_en_mem6(1)
-//  array(1).io.wr_instr_mem1 := io.wr_instr_mem1(1)
-//  array(1).io.wr_instr_mem2 := io.wr_instr_mem2(1)
-//  array(1).io.wr_instr_mem3 := io.wr_instr_mem3(1)
-//  array(1).io.wr_instr_mem4 := io.wr_instr_mem4(1)
-//  array(1).io.wr_instr_mem5 := io.wr_instr_mem5(1)
-//  array(1).io.wr_instr_mem6 := io.wr_instr_mem6(1)
-//  array(1).io.run_in := array_0.io.run_out
-//  array(1).io.PC1_in := array_0.io.PC6_out
-//  array(1).io.Addr_in := array_0.io.Addr_out
-//  for(i <- 2 until PEcolCnt){
-//    array(i).io.d_in := array(i-1).io.d_out
-//    array(i).io.wr_en_mem1 := io.wr_en_mem1(i)
-//    array(i).io.wr_en_mem2 := io.wr_en_mem2(i)
-//    array(i).io.wr_en_mem3 := io.wr_en_mem3(i)
-//    array(i).io.wr_en_mem4 := io.wr_en_mem4(i)
-//    array(i).io.wr_en_mem5 := io.wr_en_mem5(i)
-//    array(i).io.wr_en_mem6 := io.wr_en_mem6(i)
-//    array(i).io.wr_instr_mem1 := io.wr_instr_mem1(i)
-//    array(i).io.wr_instr_mem2 := io.wr_instr_mem2(i)
-//    array(i).io.wr_instr_mem3 := io.wr_instr_mem3(i)
-//    array(i).io.wr_instr_mem4 := io.wr_instr_mem4(i)
-//    array(i).io.wr_instr_mem5 := io.wr_instr_mem5(i)
-//    array(i).io.wr_instr_mem6 := io.wr_instr_mem6(i)
-//    array(i).io.run_in := array(i-1).io.run_out
-//    array(i).io.PC1_in := array(i-1).io.PC6_out
-//    array(i).io.Addr_in := array(i-1).io.Addr_out
-//  }
-//
-//  d_out := array(PEcolCnt-1).io.d_out
-//  wr_Addr_outBuf := array(PEcolCnt-1).io.Addr_out
-//  rollBack_PC := array(PEcolCnt-1).io.PC6_out
-//
-//  val allValidBits = RegInit(0.U(64.W))
-//  val allValidBitsPopCnt = RegInit(0.U(6.W))
-////  allValidBits := Cat(d_out(0).valid_a, d_out(0).valid_a, d_out(1).valid_a, d_out(1).valid_a,)
-//  allValidBits := Cat(
-//    wr_D_outBuf( 0 ).validBit,
-//    wr_D_outBuf( 1 ).validBit,
-//    wr_D_outBuf( 2 ).validBit,
-//    wr_D_outBuf( 3 ).validBit,
-//    wr_D_outBuf( 4 ).validBit,
-//    wr_D_outBuf( 5 ).validBit,
-//    wr_D_outBuf( 6 ).validBit,
-//    wr_D_outBuf( 7 ).validBit,
-//    wr_D_outBuf( 8 ).validBit,
-//    wr_D_outBuf( 9 ).validBit,
-//    wr_D_outBuf( 10 ).validBit,
-//    wr_D_outBuf( 11 ).validBit,
-//    wr_D_outBuf( 12 ).validBit,
-//    wr_D_outBuf( 13 ).validBit,
-//    wr_D_outBuf( 14 ).validBit,
-//    wr_D_outBuf( 15 ).validBit,
-//    wr_D_outBuf( 16 ).validBit,
-//    wr_D_outBuf( 17 ).validBit,
-//    wr_D_outBuf( 18 ).validBit,
-//    wr_D_outBuf( 19 ).validBit,
-//    wr_D_outBuf( 20 ).validBit,
-//    wr_D_outBuf( 21 ).validBit,
-//    wr_D_outBuf( 22 ).validBit,
-//    wr_D_outBuf( 23 ).validBit,
-//    wr_D_outBuf( 24 ).validBit,
-//    wr_D_outBuf( 25 ).validBit,
-//    wr_D_outBuf( 26 ).validBit,
-//    wr_D_outBuf( 27 ).validBit,
-//    wr_D_outBuf( 28 ).validBit,
-//    wr_D_outBuf( 29 ).validBit,
-//    wr_D_outBuf( 30 ).validBit,
-//    wr_D_outBuf( 31 ).validBit,
-//    wr_D_outBuf( 32 ).validBit,
-//    wr_D_outBuf( 33 ).validBit,
-//    wr_D_outBuf( 34 ).validBit,
-//    wr_D_outBuf( 35 ).validBit,
-//    wr_D_outBuf( 36 ).validBit,
-//    wr_D_outBuf( 37 ).validBit,
-//    wr_D_outBuf( 38 ).validBit,
-//    wr_D_outBuf( 39 ).validBit,
-//    wr_D_outBuf( 40 ).validBit,
-//    wr_D_outBuf( 41 ).validBit,
-//    wr_D_outBuf( 42 ).validBit,
-//    wr_D_outBuf( 44 ).validBit,
-//    wr_D_outBuf( 45 ).validBit,
-//    wr_D_outBuf( 46 ).validBit,
-//    wr_D_outBuf( 47 ).validBit,
-//    wr_D_outBuf( 48 ).validBit,
-//    wr_D_outBuf( 49 ).validBit,
-//    wr_D_outBuf( 50 ).validBit,
-//    wr_D_outBuf( 51 ).validBit,
-//    wr_D_outBuf( 52 ).validBit,
-//    wr_D_outBuf( 53 ).validBit,
-//    wr_D_outBuf( 54 ).validBit,
-//    wr_D_outBuf( 55 ).validBit,
-//    wr_D_outBuf( 56 ).validBit,
-//    wr_D_outBuf( 57 ).validBit,
-//    wr_D_outBuf( 58 ).validBit,
-//    wr_D_outBuf( 59 ).validBit,
-//    wr_D_outBuf( 60 ).validBit,
-//    wr_D_outBuf( 61 ).validBit,
-//    wr_D_outBuf( 62 ).validBit,
-//    wr_D_outBuf( 63 ).validBit
-//  )
-//
-//  allValidBitsPopCnt := PopCount(allValidBits)
-//
-////  when(wr_Addr_outBuf =/= 0.U) { // if Addr_out is not 0, which means there are data to be written into output buffer
-//  when(array(PEcolCnt-1).io.run_out =/= 0.U) { // if run_out signal of the last building block is not 0, there are data to be written into output buffer
-//    when(allValidBitsPopCnt === 1.U && lock =/= false.B) { // if there's only 1 output to be valid. We're good
-//      // write to output buffer
-//      // proceed
-//        outputBuffer(wr_Addr_outBuf) := wr_D_outBuf
-//    }. elsewhen(allValidBitsPopCnt === 0.U && lock =/= false.B){ // if there's no valid output. Width issue must have occurred. Output will be valid in the next cycle
-//      // write to output buffer
-//      // proceed
-//        outputBuffer(wr_Addr_outBuf) := wr_D_outBuf
-//    }. elsewhen(allValidBitsPopCnt =/= 0.U && allValidBitsPopCnt =/= 1.U && lock =/= false.B){ // if there are multiple valid outputs, depth issue is occurring, you must write back these data. The wrAddr and PC6 are carried along with the data
-//        wr_Addr_outBuf_hold := wr_Addr_outBuf
-//        lock := true.B
-//        rollBack_en := true.B
-////    }. elsewhen(allValidBitsPopCnt >= 32/*32???*/ && lock =/= false.B) {// width & depth both occurs
-////
-////    }
-//    }. otherwise{ // when you have a lock
-//      rollBack_en := false.B
-//      when(wr_Addr_outBuf_hold === wr_Addr_outBuf){
-//        lock := false.B
-//
-//      }
-//    }
-//
-//    //
-//  }
-//
-//}
 
 object BPMain extends App {
   println("Hello World, I will now generate the Verilog file!")
